c1773bff8d6aafee463833600300e4249906e6b1
Nahuel Cosme <ncdv.2003@gmail.com>
2024-01-02 18:35:28 +0000

    Horizontal line winning implemented

--- src/main/java/software/ulpgc/kata6/Board.java
+++ src/main/java/software/ulpgc/kata6/Board.java
@@ -19,6 +19,21 @@ public class Board {
     public void checkWinner() {
         if (this.state.length != 3) {
             this.winner = "";
+            return;
+        }
+        horizontalLineWinner();
+
+    }
+
+    private void horizontalLineWinner() {
+        boolean settedWinner = false;
+        for (String s : state) {
+            char mark = s.charAt(0);
+            if (s.charAt(1) == mark && s.charAt(2) == mark) {
+                this.winner = String.valueOf(mark);
+                settedWinner = true;
+                break;
+            }
         }
     }
 }
--- src/test/java/tests/BoardTest.java
+++ src/test/java/tests/BoardTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import software.ulpgc.kata6.Board;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static tests.BoardTest.Cases.*;
 
 public class BoardTest {
     @Test
@@ -13,4 +14,21 @@ public class BoardTest {
         String winner = board.winner();
         assertThat(winner).isEqualTo("");
     }
+
+    @Test
+    public void should_return_winner_given_board_3x3_horizontal_line() {
+        Board board = new Board(board_3x3_horizontal_line);
+        board.checkWinner();
+        String winner = board.winner();
+        assertThat(winner).isEqualTo("O");
+    }
+
+    public static class Cases {
+        public static String board_3x3_horizontal_line = """
+                O.X
+                OOO
+                .XX
+                """.trim();
+    }
+
 }
